d3fa6bb6bcb9f366bcef4159b0066770d12a3f16
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-01-18 08:07:34 +1100

    Reorganise repo

---
+++ multiprocessing/make_plots.py
@@ -0,0 +1,22 @@
+import h5py
+import numpy as np
+import matplotlib.pyplot as plt
+
+# load the data from weather.hdf5 here into a
+# numpy array temps (temperatures)
+
+
+def make_plot(i):
+    """Plot temperature image i with interpolation 'lanczos' and
+    save it to the corresponding {year}.png file.
+
+    Hint: use plt.imshow.
+    """
+    pass  # edit this
+
+
+if __name__ == '__main__':
+    temps = None  # edit this
+    years = None  # edit this
+    for i, year in enumerate(years):
+        make_plot(i)
