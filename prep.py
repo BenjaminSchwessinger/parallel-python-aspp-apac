673f8c70347fcf28468c2672e4b78ec7bcd5ba0a
Juan Nunez-Iglesias <juan.n@unimelb.edu.au>
2018-01-18 12:00:12 +1100

    grow dataset by interpolation instead of concatenation

--- prep.py
+++ prep.py
@@ -1,21 +1,22 @@
 import numpy as np
+from scipy import ndimage as ndi
 from skimage.transform import rescale
 import glob
 import h5py
 
-data = np.loadtxt('raw_data/2010.csv', delimiter=',')
-data = rescale(data, 10)
-nrows, ncols = data.shape
-
+data_list = []
 for filename in sorted(glob.glob('raw_data/*csv'))[1:]:
+    # upscale 10x
     tmp = rescale(np.loadtxt(filename, delimiter=','), 10, mode='reflect')
-    data = np.vstack([data, tmp])
+    data_list.append(tmp)
 
-data = data.reshape([-1, nrows, ncols])
+data = np.array(data_list)
 
-# grow dataset exponentially:
-for i in range(4):
-    data = np.vstack([data, data])
+# interpolate time dimension
+t = np.linspace(0, 7, 128, endpoint=False).reshape((-1, 1, 1))
+row_coord, col_coord = np.indices(data.shape[1:])
+indices = np.broadcast_arrays(t, row_coord, col_coord)
+full_data = ndi.map_coordinates(data, indices, mode='nearest')
 
 f = h5py.File('weather.hdf5', 'w')
-f.create_dataset("temperature", data=data)
+f.create_dataset("temperature", data=full_data)
